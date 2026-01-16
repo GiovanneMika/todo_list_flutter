7c084fa817d0d3cc5f5b7f1bda5a95d1e5db62a8
Giovanne-Vamilly <mika.2023@alunos.utfpr.edu.br>
2026-01-16 10:49:50 -0300

    feat: model de Todo e adição do Dio

--- lib/src/home_page.dart
+++ lib/src/home_page.dart
@@ -3,6 +3,14 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("Lista de Afazeres")));
+    return Scaffold(
+      appBar: AppBar(title: Text("Lista de Afazeres")),
+      body: ListView.builder(
+        itemCount: 100,
+        itemBuilder: (context, index) {
+          return ListTile(title: Text('aopa ${index + 1}'));
+        },
+      ),
+    );
   }
 }
